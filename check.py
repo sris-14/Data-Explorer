10f0bc0439f01a88a619ec0b2b42628ac77071a4
Srishti Shukla <srishtishukla781@gmail.com>
2025-03-08 11:36:36 +0530

    module import

--- check.py
+++ check.py
@@ -1,3 +1,5 @@
-import os
-print("Bio CSV Exists:", os.path.exists('D:\\Olympics-api\\Olympic_Athlete_Bio.csv'))
-print("Event CSV Exists:", os.path.exists('D:\\Olympics-api\\Olympic_Athlete_Event_Result.csv'))
+# import os
+# print("Bio CSV Exists:", os.path.exists('D:\\Olympics-api\\Olympic_Athlete_Bio.csv'))
+# print("Event CSV Exists:", os.path.exists('D:\\Olympics-api\\Olympic_Athlete_Event_Result.csv'))
+import IPython 
+print(IPython.__version__)
